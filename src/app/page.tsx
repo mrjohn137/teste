b9328c52f5261b8590eb3124b108366cd2ce4324
Johnathan Silva <johnyn137@gmail.com>
2025-05-15 18:53:42 -0300

    teste

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,18 +1,12 @@
-import { Suspense } from "react"
 
 export default async function Home() {
-  const data = await fetch('https://servicebus2.caixa.gov.br/portaldeloterias/api/lotofacil/')
-  const results = await data.json()
-
-  if (!results) {
-    return <p>Carregando</p>
-  }
-
-  console.log(results);
-
+  const data = await fetch('https://api.vercel.app/blog')
+  const posts = await data.json()
   return (
-    <Suspense fallback={<div>Loading...</div>}>
-      {results.numero}
-    </Suspense>
-  );
+    <ul>
+      {posts.map((post) => (
+        <li key={post.id}>{post.title}</li>
+      ))}
+    </ul>
+  )
 }
