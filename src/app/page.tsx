1c9c751c054dbf0f989993171a31ec72610d9588
Johnathan Silva <johnyn137@gmail.com>
2025-05-16 13:26:11 -0300

    sercvvee

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,67 +1,22 @@
-export interface Root {
-  acumulado: boolean
-  dataApuracao: string
-  dataProximoConcurso: string
-  dezenasSorteadasOrdemSorteio: string[]
-  exibirDetalhamentoPorCidade: boolean
-  id: number
-  indicadorConcursoEspecial: number
-  listaDezenas: string[]
-  listaDezenasSegundoSorteio: string
-  listaMunicipioUFGanhadores: string[]
-  listaRateioPremio: ListaRateioPremio[]
-  listaResultadoEquipeEsportiva: string
-  localSorteio: string
-  nomeMunicipioUFSorteio: string
-  nomeTimeCoracaoMesSorte: string
-  numero: number
-  numeroConcursoAnterior: number
-  numeroConcursoFinal_0_5: number
-  numeroConcursoProximo: number
-  numeroJogo: number
-  observacao: string
-  premiacaoContingencia: number
-  tipoJogo: string
-  tipoPublicacao: number
-  ultimoConcurso: boolean
-  valorArrecadado: number
-  valorAcumuladoConcurso_0_5: number
-  valorAcumuladoConcursoEspecial: number
-  valorAcumuladoProximoConcurso: number
-  valorEstimadoProximoConcurso: number
-  valorSaldoReservaGarantidora: number
-  valorTotalPremioFaixaUm: number
+import React from "react";
+
+export interface PostsProps {
+  numero: string
+}
+async function getPosts() {
+  const res = await fetch("https://servicebus2.caixa.gov.br/portaldeloterias/api/lotofacil/");
+  return res.json();
 }
 
-export interface ListaRateioPremio {
-  descricaoFaixa: string
-  faixa: number
-  numeroDeGanhadores: number
-  valorPremio: number
-}
+const post: PostsProps = await getPosts();
 
-async function getData() {
-  const res = await fetch(`https://servicebus2.caixa.gov.br/portaldeloterias/api/lotofacil/`,
-    {
-      cache: 'no-store',
-      next: { revalidate: 60 }
-    }
-  )
-
-  return res.json()
-}
-
-
-
-export default async function Page() {
-  const data: Root = await getData()
-  if (!data) {
-    return <p>Carregando...</p>
-  }
+export default async function PostsPage() {
   return (
-    <>
-      <h1>Home</h1>
-      <h2>{data.numero}</h2>
-    </>
-  )
+    <main>
+      <h1>Posts archive</h1>
+      <ol>
+        <li key={post.numero}>{post.numero}</li>
+      </ol>
+    </main>
+  );
 }
